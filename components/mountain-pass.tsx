82155008c5d7496efe5d798810970f2ef08e5a66
Asle Berge <asle.berge@gmail.com>
2022-02-28 10:54:42 +0100

    Add prettier and loading for mountain-pass component.

--- components/mountain-pass.tsx
+++ components/mountain-pass.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
+import React from 'react';
 import { useQuery } from 'react-query';
-import { YR_BASE_URL } from '../constants';
 import { performRequest } from '../http';
 import { YrResponse } from '../yr-response';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
+// TODO: Hent data på ny med refetch knapp. Må ha en cooldown peridoe. Hvor lenge skal det være cachet?
+// TODO: Style finere. bakgrunn #111111, kort: enten #000000 eller #333333
+// TODO: Trenger en farge til knapp også.
 
 const MountainPass: React.FC = () => {
-    const { data } = useQuery('yr', () => performRequest<null,YrResponse>('/api/mountain-pass'));
+    const { data, isFetching, isLoading } = useQuery('yr', () =>
+        performRequest<null, YrResponse>('/api/mountain-pass')
+    );
 
-    if (!data) return <p className={styles.description}>Noe gikk galt!</p>
+    if (isFetching || isLoading ) return <p className={styles.description}>
+        Henter data 📀
+    </p>
 
-    const { _embedded: { route, roadMessages} } = data;
-  return (
-      <>
-        <h1 className={styles.title}>
-            Er Hunnedalen stengt?
-        </h1>
-        <h2 className={styles.title}>
-            {route.isClosed ? 'JA!' : 'Nei'}
-        </h2>
+    if (!data) return <p className={styles.description}>Noe gikk galt!</p>;
+
+    const {
+        _embedded: { route, roadMessages },
+    } = data;
+    return (
         <>
-        { roadMessages 
-            && roadMessages.length > 0 
-                ? roadMessages.map(m => (
-                    <p key={m.id}>
-                    {m.message}
-                    </p>
-                ))
-                : (
+            <h1 className={styles.title}>Er Hunnedalen stengt?</h1>
+            <h2 className={styles.title}>{route.isClosed ? 'JA!' : 'Nei'}</h2>
+            <>
+                {roadMessages && roadMessages.length > 0 ? (
+                    roadMessages.map((m) => <p key={m.id}>{m.message}</p>)
+                ) : (
                     <p className={styles.description}>
-                    Det er ikke kommet noen meldinger 
-                    om at Fylkesvei 45 mellom Ålgård og Sirdal er stengt.
+                        Det er ikke kommet noen meldinger om at Fylkesvei 45
+                        mellom Ålgård og Sirdal er stengt.
                     </p>
-                    )
-        }
+                )}
+            </>
         </>
-      </>
-  )
-}
+    );
+};
 
 export default MountainPass;
